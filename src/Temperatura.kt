7a7cf175672aebdee51a5916bbb15abc53cbbf80
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-12 23:29:18 -0300

    Verifica temperatura negativa

---
+++ src/Temperatura.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var temp1: Int = -1
+    var temp2: Int = -5
+    var temp3: Int = 25
+
+    if(temp1 < 0 || temp2 < 0 || temp3 < 0){
+        println("Alguma temperatura é negativa")
+    }
+    else{
+        println("Não há temperaturas negativas")
+}
