5318b465b1d20649a5fdcec4198399e185db4b8c
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-12 23:29:37 -0300

    Tipos numéricos

---
+++ src/TiposNumericos.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var numero: Double = 3.58585848181
+    var numero2: Float = 5.3f
+    var numeroInteiro: Int = 10
+    var numeroLong: Long = 100000000L
+    var resultado: Boolean = false
+    
+    println(numero)
+    println(numero2)
+    println(numeroInteiro)
+    println(numeroLong)
+    println(resultado)
+}
