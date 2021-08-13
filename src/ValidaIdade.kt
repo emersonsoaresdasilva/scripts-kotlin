6beb93a138bb493e5483640e4780460983eb002f
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-12 23:29:59 -0300

    Valida idade de pessoa

---
+++ src/ValidaIdade.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var idade: Int = 20
+    
+    if(idade >= 20){
+        println("Adulto")
+    }
+    else if (idade >= 14 && idade <= 17){
+        println("Adolescente")
+    }
+    else if (idade<14){
+        println("Criança")
+    }
+}
