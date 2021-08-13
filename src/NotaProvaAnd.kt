23051a812d8e640635afc733c27d2ddc78fe88c6
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-12 23:21:44 -0300

    Compara notas de prova

---
+++ src/NotaProvaAnd.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var notaProfessor: Int = 7
+	var notaProvaGeral: Int = 7
+    
+    println(notaProfessor>6 && notaProvaGeral>6)
+}
---
+++ src/NotaProvaOr.kt
@@ -0,0 +1,6 @@
+fun main(){       
+	var notaProfessor: Int = 6
+	var notaProvaGeral: Int = 6
+ 
+	println(notaProfessor>6 || notaProvaGeral>6)
+}
