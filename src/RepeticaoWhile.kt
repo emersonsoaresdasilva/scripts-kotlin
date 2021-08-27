dd850d3eb539b90c592409b61a7c9b1c7bfe2b7e
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-26 22:27:05 -0300

    Estrutura de repetição (while)

---
+++ src/RepeticaoWhile.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var i = 0
+
+    while(i < 10){
+        if(i < 9){
+            println("$i - ")
+        }
+        else{
+            println(i)
+        }
+    }
+}
---
+++ src/RepeticaoWhile2.kt
@@ -0,0 +1,11 @@
+// Apresente todos os números ímpares entre 1 a 100 utilizando o While.
+
+fun main(){
+    var i = 0;
+    while (i < 100){
+        if(i % 2 != 0){
+            println(i)
+        }
+    i++
+    }
+}
