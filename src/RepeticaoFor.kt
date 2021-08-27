9dd2ee8bdbf14fe440b4b5fad9594894d8764cdb
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-26 22:26:33 -0300

    Estrutura de repetição (for)

---
+++ src/RepeticaoFor.kt
@@ -0,0 +1,5 @@
+fun main(){
+    for(valor in 1..10){
+        println(valor)
+    }
+}
---
+++ src/RepeticaoFor2.kt
@@ -0,0 +1,19 @@
+// Apresente todos os números pares entre 1 a 100, utilizando o FOR.
+
+fun main(){
+    for (valor in 1..100){
+        if (valor % 2 == 0){
+            println(valor)
+            }
+        }
+}
+
+// Percorra todos os números de 1 a 100 e apresente quais números são divisíveis por 5.
+
+fun main(){
+    for (numero in 1..100){
+        if (numero % 5 == 0){
+            println(numero)
+            }
+        }
+}
